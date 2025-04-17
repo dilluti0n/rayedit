ab86686806d5e031dc532ec2c04fae473b23ddc9
dilluti0n <hskimse1@gmail.com>
2025-04-18 00:33:56 +0900

    Implement simple real time interactive text rendering
    Dependancy: raylib-5.5_linux_amd64[1]
    
    [1] https://github.com/raysan5/raylib/releases/tag/5.5

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <raylib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+
+#define STR_LEN 255
+
+#ifndef MAIN_WINDOW_TITLE
+#define MAIN_WINDOW_TITLE "window"
+#endif
+
+const int INIT_WIDTH = 800;
+const int INIT_HEIGHT = 600;
+
+Vector2 window_size = {.x = INIT_WIDTH, .y = INIT_HEIGHT};
+
+void CustomLogCallback(int logLevel, const char *text, va_list args) {
+    FILE *out = stderr;
+    fprintf(out, "[%s] ", (logLevel == LOG_INFO) ? "INFO" :
+                         (logLevel == LOG_WARNING) ? "WARN" :
+                         (logLevel == LOG_ERROR) ? "ERR" : "DEBUG");
+
+    vfprintf(out, text, args);
+    fprintf(out, "\n");
+}
+
+int main() {
+	SetTraceLogCallback(CustomLogCallback);
+	InitWindow(window_size.x, window_size.y, MAIN_WINDOW_TITLE);
+        SetWindowState(FLAG_WINDOW_RESIZABLE);
+        EnableEventWaiting();
+
+        char str[STR_LEN] = {};
+        size_t strp = 0;
+
+        while (!WindowShouldClose()) {
+		if (IsWindowResized()) {
+			window_size.x = GetScreenWidth();
+			window_size.y = GetScreenHeight();
+                }
+
+                {
+			int c;
+			while ((c = GetCharPressed())) {
+				if (strp >= STR_LEN - 1) {
+					strp = 0;
+				}
+				str[strp++] = c;
+				str[strp] = '\0';
+                        }
+                }
+
+                if (IsKeyPressed(KEY_ENTER)) {
+			str[strp] = '\n'; /* we can ensure strp < STR_LEN - 1 */
+			assert(write(STDOUT_FILENO, str, strp + 1) >= 0 );
+			strp = 0;
+			str[strp] = '\0';
+                } else if (IsKeyPressed(KEY_BACKSPACE) && strp > 0) {
+			str[--strp] = '\0';
+                }
+
+                {
+			int font_size = 20;
+			int text_width = MeasureText(str, font_size);
+
+			BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText(str,
+				 (window_size.x - text_width) / 2, (window_size.y - font_size) / 2,
+				 font_size, LIGHTGRAY);
+			EndDrawing();
+		}
+	}
+	CloseWindow();
+}
---
+++ makefile
@@ -0,0 +1,20 @@
+TARGET=rayedit
+RAYLIB_PREFIX="./raylib-5.5_linux_amd64"
+CC=gcc
+CFLAGS=-Wall -O3 -I${RAYLIB_PREFIX}/include -DMAIN_WINDOW_TITLE="\"${TARGET}\""
+# LDFLAGS=-v
+LDLIBS=${RAYLIB_PREFIX}/lib/libraylib.a -lm
+OBJS=main.o
+
+.PHONY=clean all
+
+all: ${TARGET}
+
+${TARGET}: ${OBJS}
+	${CC} ${LDFLAGS} -o ${TARGET} ${OBJS} ${LDLIBS}
+
+%.o: %.c makefile
+	${CC} ${CFLAGS} -c $< -o $@
+
+clean:
+	rm -rf ${OBJS} ${TARGET}
