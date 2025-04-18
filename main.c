7d8d3f13e240f9763ae543d39433ff8ba52c775b
dilluti0n <hskimse1@gmail.com>
2025-04-18 09:30:39 +0900

    tabify, gitignore, add -Wextra to CFLAGS

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ Vector2 window_size = {.x = INIT_WIDTH, .y = INIT_HEIGHT};
 void CustomLogCallback(int logLevel, const char *text, va_list args) {
 	FILE *out = stderr;
 	fprintf(out, "[%s] ", (logLevel == LOG_INFO) ? "INFO" :
-			      (logLevel == LOG_WARNING) ? "WARN" :
-			      (logLevel == LOG_ERROR) ? "ERR" : "DEBUG");
+		(logLevel == LOG_WARNING) ? "WARN" :
+		(logLevel == LOG_ERROR) ? "ERR" : "DEBUG");
 
 	vfprintf(out, text, args);
 	fprintf(out, "\n");
@@ -33,9 +33,9 @@ int main() {
 	SetWindowState(FLAG_WINDOW_RESIZABLE);
 	EnableEventWaiting();
 
-        struct lines *lines;
-        struct line *line;
-        lines_init(&lines);
+	struct lines *lines;
+	struct line *line;
+	lines_init(&lines);
 	line_init(&line);
 
 	while (!WindowShouldClose()) {
@@ -66,16 +66,16 @@ int main() {
 			ClearBackground(RAYWHITE);
 			/* DrawText(lineh->vec->data, */
 			/*	 (window_size.x - text_width) / 2, 0, */
-                        /*	 font_size, BLACK); */
-                        int i;
-                        for (i = 0; i < lines->arr->size; i++) {
+			/*	 font_size, BLACK); */
+			size_t i;
+			for (i = 0; i < lines->arr->size; i++) {
 				struct line *p = VEC_LINE_get(lines->arr, i);
 				DrawText(p->vec->data, 10, 10 + font_size * i, font_size, BLACK);
-                        }
-                        DrawText(line->vec->data, 10, 10 + font_size * i, font_size, BLACK);
+			}
+			DrawText(line->vec->data, 10, 10 + font_size * i, font_size, BLACK);
 			EndDrawing();
 		}
 	}
-        CloseWindow();
-        lines_free(lines);
+	CloseWindow();
+	lines_free(lines);
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 TARGET=rayedit
 RAYLIB_PREFIX="./raylib-5.5_linux_amd64"
 CC=gcc
-CFLAGS=-Wall -O3 -I${RAYLIB_PREFIX}/include -DMAIN_WINDOW_TITLE="\"${TARGET}\""
+CFLAGS=-Wall -Wextra -O3 -I${RAYLIB_PREFIX}/include -DMAIN_WINDOW_TITLE="\"${TARGET}\""
 # LDFLAGS=-v
 LDLIBS=${RAYLIB_PREFIX}/lib/libraylib.a -lm
 OBJS=main.o line.o lines.o
