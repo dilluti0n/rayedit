faab906634aeb40da7a554566179ffdfd8c0a5e7
dilluti0n <hskimse1@gmail.com>
2025-04-18 12:57:10 +0900

    implement unit test for vector::init_and_free, vector::grow

--- test/test_line.c
+++ test/test_line.c
@@ -1,8 +1,56 @@
 #include <criterion/criterion.h>
 #include <criterion/assert.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+
 #include "../line.h"
+#include "../vector.h"
+
+DEFINE_VECTOR(Vec_test, char);
+
+Test(vector_suit, init_and_free) {
+	Vec_test *v = malloc(sizeof(Vec_test));
+	assert(v != NULL);
+	Vec_test_init(v);
+	cr_assert_eq(v->size, 0);
+	cr_assert_eq(v->capacity, VECTOR_INIT_CAP);
+	cr_assert_not_null(v->data);
+	Vec_test_free(v);
+	free(v);
+}
+
+Test(vector_suit, grow) {
+	Vec_test *v = malloc(sizeof(Vec_test));
+	assert(v != NULL);
+	Vec_test_init(v);
+	size_t track = VECTOR_INIT_CAP;
+	const size_t MAX_GROWTH = 1 << 22;
+
+	memset(v->data, 0, track);
+	while (track < MAX_GROWTH) {
+		Vec_test_grow(v);
+		for (size_t i = 0; i < track; i++)
+			cr_assert_eq(v->data[i], 0,
+				     "Check v->data data integrity when grow (track=%zu, i=%zu)",
+				     track, i);
+		cr_assert_eq(v->capacity, track * 2);
+		memset(v->data + track, 0, track);
+		track *= 2;
+	}
+	Vec_test_free(v);
+	free(v);
+}
+
+Test(line_suite, init_and_free) {
+	struct line *li;
+
+	line_init(&li);
+	cr_assert_neq(li->vec, NULL);
+	cr_assert_eq(li->cursor, 0);
+	cr_assert_eq(li->last, 0);
+	line_free(li);
+}
 
 Test(line_suite, append) {
 	struct line *li;
