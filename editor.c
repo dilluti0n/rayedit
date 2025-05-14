dilluti0n/rayedit

commit 2441e78bffaef89dcea64c547329e541250553e7

fix: Memory Leak in eb_delete_line
The function eb_delete_line did not properly free the contents of the
entry when deleting an element from eb->line_vec. This patch fixes
that issue.

editor.c
@@ -57,8 +57,34 @@ static inline struct line *eb_get_line(const struct ed_buf *eb, size_t index) {
 	return Vec_slinep_get(eb->line_vec, index);
 }
 
+#ifdef DEBUG
+static void print_eb(struct ed_buf *eb) {
+	printf("eb->line_vec: %p\n\
+eb->cur_row: %lu\n\
+eb->cur_col: %lu\n\
+eb->scroll_row: %lu\n\
+eb->file_name: %s\n\
+eb->raw: %p\n\
+eb->raw_size: %lu\n",
+	       eb->line_vec,
+	       eb->cur_row,
+	       eb->cur_col,
+	       eb->scroll_row,
+	       eb->file_name,
+	       eb->raw,
+	       eb->raw_size);
+}
+#endif
+
+#ifdef DEBUG
+#define PRINT_EB(eb) print_eb(eb)
+#else
+#define PRINT_EB(eb) ((void)0)
+#endif
+
 static inline struct line *ensure_line(struct ed_buf *eb, size_t row) {
 	ASSERT(row <= Vec_slinep_len(eb->line_vec));
+	PRINT_EB(eb);
 	struct line *li;
 	if (row == Vec_slinep_len(eb->line_vec)) {
 		line_init(&li);
@@ -69,6 +95,12 @@ static inline struct line *ensure_line(struct ed_buf *eb, size_t row) {
 	li = Vec_slinep_get(eb->line_vec, row);
 	if (li == NULL) {
 		line_init(&li);
+
+		struct line *oldli = Vec_slinep_get(eb->line_vec, row);
+
+		if (oldli != NULL)
+			line_free(oldli);
+
 		Vec_slinep_set(eb->line_vec, row, li);
 	}
 	return li;
@@ -76,23 +108,23 @@ static inline struct line *ensure_line(struct ed_buf *eb, size_t row) {
 
 /* insert ch to cursor */
 void eb_insert(struct ed_buf *eb, int ch) {
-#ifdef DEBUG
-	assert(eb != NULL);
-#endif
+	ASSERT(eb != NULL);
 	struct line *line = ensure_line(eb, eb->cur_row);
 	line_insert(line, eb->cur_col++, ch);
 }
 
 void eb_delete_line(struct ed_buf *eb, size_t pos) {
 	ASSERT(pos <= Vec_slinep_len(eb->line_vec));
-	if (pos < Vec_slinep_len(eb->line_vec))
+	if (pos < Vec_slinep_len(eb->line_vec)) {
+		struct line *li;
+		if ((li = Vec_slinep_get(eb->line_vec, pos)) != NULL)
+			line_free(li);
 		Vec_slinep_delete(eb->line_vec, pos);
+	}
 }
 
 void eb_backspace(struct ed_buf *eb) {
-#ifdef DEBUG
-	assert(eb != NULL);
-#endif
+	ASSERT(eb != NULL);
 	struct line *curr = eb_get_line(eb, eb->cur_row);
 	if (eb->cur_col == 0) {	      /* backspace to upper line */
 		if (eb->cur_row == 0) /* nothing to remove */
@@ -301,4 +333,5 @@ void eb_save_file(struct ed_buf *eb) {
 		perror("rename: ");
 		unlink(tmp_path);
 	}
+	printf("saved to: %s\n", eb->file_name);
 }

line.c
@@ -3,6 +3,8 @@
 #include "raylib.h"
 #include "line.h"
 
+#include <stdio.h>
+
 DEFINE_VECTOR(Vec_char, char);
 
 struct line {
@@ -19,9 +21,6 @@ void line_init(struct line **lip) {
 	li->is_lazy = 0;
 	Vec_char_push(li->vec, '\0');
 	*lip = li;
-#ifdef DEBUG
-	printf("new line %p allocated\n", li);
-#endif
 }
 
 void line_lazy_init(struct line **lip, const char *origin, size_t len) {
@@ -70,6 +69,7 @@ void line_init_from_buf(struct line **lip, const char *buf, size_t len) {
 static inline void edit_happen(struct line *li) {
 	if (li->is_lazy) {
 		ASSERT(li->origin != NULL);
+		ASSERT(li->vec == NULL);
 		li->vec = produce_vec_from_buf(li->origin, li->origin_len);
 		li->is_lazy = 0;
 	}
@@ -163,7 +163,7 @@ void line_cat(struct line *dest, const struct line *src) {
 	ASSERT(src != NULL);
 
 	if (line_get_last(src) == 0)
-		return; 	/* trivial cat */
+		return;		/* trivial cat */
 
 	edit_happen(dest);
 
@@ -179,9 +179,9 @@ void line_cat(struct line *dest, const struct line *src) {
 
 void line_free(struct line *li) {
 	ASSERT(li != NULL);
-	if (!li->is_lazy) {
-		ASSERT(li->vec != NULL);
+
+	if (li->vec != NULL)
 		Vec_char_free(li->vec);
-	}
+
 	MemFree(li);
 }

vector.h
@@ -113,14 +113,14 @@
 		memcpy(to_insert, src->data,				\
 		       src->size * sizeof (type));			\
 	}								\
-        static inline void name##_cat(name *dest, const name *src) {    \
+	static inline void name##_cat(name *dest, const name *src) {    \
 		const size_t oldsize = dest->size;			\
 		const size_t newsize = oldsize + src->size;		\
 		name##_resize(dest, oldsize + src->size);		\
 		memcpy(dest->data + oldsize, src->data,			\
 		       src->size * sizeof (type));			\
 		dest->size = newsize;					\
-	}							        \
+	}								\
 	static inline void name##_cat_raw(name *dest, const type *src,	\
 					  size_t src_len) {		\
 		const size_t oldsize = dest->size;			\
@@ -129,7 +129,7 @@
 		memcpy(dest->data + oldsize, src,			\
 		       src_len * sizeof (type));			\
 		dest->size = newsize;					\
-	}							        \
+	}								\
 	/* split from index and store it to *new */			\
 	static inline void name##_split(name *v, size_t index, name **new) { \
 		ASSERT(index < v->size);				\

test_editor.c
@@ -88,21 +88,91 @@ Test(editor_suite, not_middle_of_line) {
 }
 
 Test(editor_suite, cursor_move) {
-	struct ed_buf *eb = NULL;
-
-	eb_init(&eb);
-
 	cr_assert_fail("TODO");
-
-	eb_free(eb);
 }
 
+
+// TODO: Test editing operations in the middle of a line
 Test(editor_suite, middle_of_line) {
 	struct ed_buf *eb = NULL;
+	struct slice sl = {};
 
 	eb_init(&eb);
+	cr_assert_eb_eq(eb, 0, 0);
+	cr_assert_eq(eb_get_line_num(eb), 0);
 
-	cr_assert_fail("TODO");
+	// Create initial line: "abcdef"
+	eb_insert(eb, 'a'); eb_insert(eb, 'b'); eb_insert(eb, 'c');
+	eb_insert(eb, 'd'); eb_insert(eb, 'e'); eb_insert(eb, 'f');
+	cr_assert_eb_eq(eb, 0, 6);
+	cr_assert_eq(eb_get_line_num(eb), 1);
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abcdef");
+
+	// Move cursor to the middle (after 'c', col 3)
+	eb_set_cur_backward(eb); // @ 0,5
+	eb_set_cur_backward(eb); // @ 0,4
+	eb_set_cur_backward(eb); // @ 0,3
+	cr_assert_eb_eq(eb, 0, 3);
+
+	// Insert 'X' at (0, 3)
+	eb_insert(eb, 'X'); // "abcXdef"
+	cr_assert_eb_eq(eb, 0, 4); // Cursor moved to after 'X'
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abcXdef");
+
+	// Insert 'Y' at (0, 4) (which is between 'X' and 'd')
+	eb_insert(eb, 'Y'); // "abcXYdef"
+	cr_assert_eb_eq(eb, 0, 5); // Cursor moved after 'Y'
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abcXYdef");
+
+	// Move cursor back to (0, 3) (between 'c' and 'X')
+	eb_set_cur_backward(eb); // @ 0,4 (after 'X')
+	eb_set_cur_backward(eb); // @ 0,3 (after 'c')
+	cr_assert_eb_eq(eb, 0, 3);
+
+	// Backspace from (0, 3) (deletes character BEFORE cursor: 'c')
+	eb_backspace(eb); // "abXYdef"
+	cr_assert_eb_eq(eb, 0, 2); // Cursor moved to after 'b'
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abXYdef");
+
+	// Backspace from (0, 2) (deletes 'b')
+	eb_backspace(eb); // "aXYdef"
+	cr_assert_eb_eq(eb, 0, 1); // Cursor at (0, 1)
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "aXYdef");
+
+	// Backspace from (0, 1) (deletes 'a')
+	eb_backspace(eb); // "XYdef"
+	cr_assert_eb_eq(eb, 0, 0); // Cursor at (0, 0)
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "XYdef");
+
+	// Test newline in the middle
+	eb_free(eb); // Reset buffer
+	eb_init(&eb);
+	eb_insert(eb, 'a'); eb_insert(eb, 'b'); eb_insert(eb, 'c');
+	eb_insert(eb, 'd'); eb_insert(eb, 'e'); // "abcde" @ 0,5
+	cr_assert_eb_eq(eb, 0, 5);
+	cr_assert_eq(eb_get_line_num(eb), 1);
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abcde");
+
+	// Move cursor to (0, 3) (after 'c')
+	eb_set_cur_backward(eb); // @ 0,4
+	eb_set_cur_backward(eb); // @ 0,3
+	cr_assert_eb_eq(eb, 0, 3);
+
+	// Call newline at (0, 3)
+	eb_newline(eb); // "abc" \n "de"
+	cr_assert_eb_eq(eb, 1, 0); // Cursor moves to start of new line
+	cr_assert_eq(eb_get_line_num(eb), 2); // Should now have 2 lines
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abc");
+	eb_get_line_slice(eb, 1, &sl); cr_assert_slice_eq(sl, "de");
+
+	// Test backspace (line join) from the start of line 1
+	// Cursor is currently at (1, 0)
+	eb_backspace(eb); // Joins line 1 ("de") to line 0 ("abc") -> "abcde"
+	// Cursor moves to the end of the original line 0, which was length 3
+	cr_assert_eb_eq(eb, 0, 3); // Cursor at (0, 3)
+	cr_assert_eq(eb_get_line_num(eb), 1); // Back to 1 line
+	eb_get_line_slice(eb, 0, &sl); cr_assert_slice_eq(sl, "abcde");
+
 
 	eb_free(eb);
 }

main.c
@@ -110,7 +110,6 @@ int main(int argc, char *argv[]) {
 #endif
 
 			for (size_t i = 0; i <= linenum_to_draw; i++) {
-				/* TODO: cannot draw cursor if it is on non_allocated line */
 				struct slice sl = {};
 				eb_get_line_slice(eb, i, &sl);
 #ifdef DEBUG

makefile
@@ -7,7 +7,7 @@ LDLIBS=${RAYLIB_PREFIX}/lib/libraylib.a -lm
 OBJS = main.o line.o editor.o
 
 ifeq ($(DEBUG), 1)
-CFLAGS += -DDEBUG -ggdb -march=x86-64 -mtune=generic -fsanitize=address
+CFLAGS += -DDEBUG -ggdb
 endif
 
 .PHONY=clean all
@@ -30,6 +30,8 @@ editor.o: editor.c line.h slice.h makefile
 TEST_TARGET=unittest
 TEST_PREFIX=./test
 
+TEST_CFLAGS = -ggdb -march=x86-64 -mtune=generic -fsanitize=address
+
 test: ${TEST_TARGET}
 	./${TEST_TARGET}
 
@@ -37,8 +39,7 @@ TEST_SRCS=test_vector.c test_line.c test_editor.c
 TEST_OBJS=$(filter-out main.o, $(OBJS))
 
 ${TEST_TARGET}: ${TEST_SRCS} ${TEST_OBJS}
-	${CC} ${CFLAGS} ${TEST_SRCS} ${TEST_OBJS} -o $@ -lcriterion ${LDLIBS}
+	${CC} ${CFLAGS} ${TEST_CFLAGS} ${TEST_SRCS} ${TEST_OBJS} -o $@ -lcriterion ${LDLIBS}
 
 clean:
 	rm -rf ${OBJS} ${TARGET} ${TEST_TARGET}
-

test_vector.c
@@ -23,15 +23,10 @@ Test(vector_suite, grow) {
 	size_t track = VECTOR_INIT_CAP;
 	const size_t MAX_GROWTH = 1 << 20;
 
-	memset(v->data, 0, track);
+	/* TODO; this is totally wrong... */
 	while (track < MAX_GROWTH) {
 		Vec_test_grow(v);
-		for (size_t i = 0; i < track; i++)
-			cr_assert_eq(v->data[i], 0,
-				     "Check v->data data integrity when grow (track=%zu, i=%zu)",
-				     track, i);
 		cr_assert_eq(v->capacity, track * 2);
-		memset(v->data + track, 0, track);
 		track *= 2;
 	}
 	Vec_test_free(v);
@@ -420,7 +415,7 @@ Test(vector_suite, delete_range) {
 	/* ---- 3) Remove the last (and only) element: index 0, n = 1 */
 	Vec_test_deleten(v, 0, 1);                /* {} */
 	cr_assert_eq(Vec_test_len(v), 0,
-                     "Vector should be empty after deleting final element");
+		     "Vector should be empty after deleting final element");
 
 	Vec_test_free(v);
 }

commit 502aea93b7e2cae6a17d83e6989109d56c9ec79f

fix: double-free of line object in eb_backspace and eb_delete_line
On eb_backspace, when the upper line was non-null, line_cat followed
by line_free, and eb_delete_line only cleared the line_vec. However,
commit 2441e78 introduced an additional line_free in eb_delete_line to
fix a memory leak, which led to a double-free for that line. This
patch resolves that problem.

editor.c
@@ -11,6 +11,7 @@
 #include "editor.h"
 #include "line.h"
 #include "vector.h"
+#include "log.h"
 
 #include "config.h"
 
@@ -60,7 +61,7 @@ static inline struct line *eb_get_line(const struct ed_buf *eb, size_t index) {
 
 #ifdef DEBUG
 static void print_eb(struct ed_buf *eb) {
-	printf("eb->line_vec: %p\n\
+	log_printf(RED_LOG_DEBUG, "eb->line_vec: %p\n\
 eb->cur_row: %lu\n\
 eb->cur_col: %lu\n\
 eb->scroll_row: %lu\n\
@@ -75,12 +76,26 @@ eb->raw_size: %lu\n",
 	       eb->raw,
 	       eb->raw_size);
 }
+
+
+static void print_eb_vec(struct ed_buf *eb) {
+	if (eb->line_vec != NULL) {
+		size_t len = Vec_slinep_len(eb->line_vec);
+		for (size_t i = 0; i < len; i++) {
+			log_printf(RED_LOG_DEBUG,
+				   "line %u, %p\n",
+				   i, Vec_slinep_get(eb->line_vec, i));
+		}
+	}
+}
 #endif
 
 #ifdef DEBUG
 #define PRINT_EB(eb) print_eb(eb)
+#define PRINT_EB_VEC(eb) print_eb_vec(eb)
 #else
 #define PRINT_EB(eb) ((void)0)
+#define PRINT_EB_VEC(eb) ((void)0)
 #endif
 
 static inline struct line *ensure_line(struct ed_buf *eb, size_t row) {
@@ -116,6 +131,7 @@ void eb_insert(struct ed_buf *eb, int ch) {
 
 void eb_delete_line(struct ed_buf *eb, size_t pos) {
 	ASSERT(pos <= Vec_slinep_len(eb->line_vec));
+	PRINT_EB_VEC(eb);
 	if (pos < Vec_slinep_len(eb->line_vec)) {
 		struct line *li;
 		if ((li = Vec_slinep_get(eb->line_vec, pos)) != NULL)
@@ -139,7 +155,6 @@ void eb_backspace(struct ed_buf *eb) {
 			const size_t upper_last = line_get_last(upper);
 			if (line_get_last(curr) > 0) {
 				line_cat(upper, curr);
-				line_free(curr);
 			}
 			eb_delete_line(eb, eb->cur_row);
 			eb->cur_col = upper_last;

line.c
@@ -178,7 +178,7 @@ void line_cat(struct line *dest, const struct line *src) {
 void line_free(struct line *li) {
 	ASSERT(li != NULL);
 
-	if (li->vec != NULL)
+	if (!li->is_lazy)
 		Vec_char_free(li->vec);
 
 	mem_free(li);

vector.h
@@ -23,7 +23,7 @@
 		nv->size = 0;						\
 		nv->capacity = VECTOR_INIT_CAP;				\
 		const size_t to_alloc = nv->capacity * sizeof(type);	\
-		nv->data = mem_malloc(to_alloc);				\
+		nv->data = mem_malloc(to_alloc);			\
 		*v = nv;						\
 	}								\
 	static inline void name##_init_with_capacity(name **v, size_t cap) { \
@@ -31,7 +31,7 @@
 		nv->size = 0;						\
 		nv->capacity = cap;					\
 		const size_t to_alloc = nv->capacity * sizeof(type);	\
-		nv->data = mem_malloc(to_alloc);				\
+		nv->data = mem_malloc(to_alloc);			\
 		*v = nv;						\
 	}								\
 	static inline void name##_clear(name *v) {			\
@@ -48,7 +48,7 @@
 		v->capacity *= 2;					\
 									\
 		const size_t to_alloc = v->capacity * sizeof(type);	\
-		type *nd = mem_malloc(to_alloc);				\
+		type *nd = mem_malloc(to_alloc);			\
 									\
 		memcpy(nd, v->data, v->size * sizeof(type));		\
 		mem_free(v->data);					\
@@ -59,7 +59,7 @@
 			while (size > v->capacity)			\
 				v->capacity *= 2;			\
 			const size_t to_alloc = v->capacity * sizeof(type); \
-			type *nd = mem_malloc(to_alloc);			\
+			type *nd = mem_malloc(to_alloc);		\
 									\
 			memcpy(nd, v->data, v->size * sizeof(type));	\
 			mem_free(v->data);				\

test_editor.c
@@ -7,6 +7,7 @@
 #include "editor.h"
 
 #include "vector.h"
+#include "log.h"
 
 DEFINE_VECTOR(Vec_slinep, struct line *);
 
@@ -96,6 +97,8 @@ Test(editor_suite, cursor_move) {
 Test(editor_suite, middle_of_line) {
 	struct ed_buf *eb = NULL;
 	struct slice sl = {};
+
+	log_init(RED_LOG_ALL, stdout);
 
 	eb_init(&eb);
 	cr_assert_eb_eq(eb, 0, 0);

test_line.c
@@ -18,7 +18,7 @@ struct line {
 
 #include "line.h"
 
-#define STR(li)   line_get_string(li)   /* shorthand        */
+#define STR(li)   line_get_string(li)   /* shorthand */
 
 /*----------------------------------------------------------------------
   1) Construction and basic invariants