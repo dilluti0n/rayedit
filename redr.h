5f8b3722f29867a87eea5717421b03c80c6f4d8f
dilluti0n <hskimse1@gmail.com>
2025-06-01 20:51:20 +0900

    minor: redefinition on redr.h

--- redr.h
+++ redr.h
@@ -42,15 +42,13 @@ struct redr_ctx;
 void redr_init(struct redr_ctx **ctxp, float width, float height,
 	       const char *title);
 void redr_draw_text(struct redr_ctx *ctx, const char *text,
-		    int x, int y, int fontSize, struct redr_color color);
+		    int x, int y, int font_size, struct redr_color color);
 bool redr_should_close(struct redr_ctx *ctx);
 float redr_width(struct redr_ctx *ctx);
 float redr_height(struct redr_ctx *ctx);
 bool redr_is_resized(struct redr_ctx *ctx);
 void redr_resize(struct redr_ctx *ctx, float width, float height);
 void redr_begin_draw(struct redr_ctx *ctx);
-void redr_draw_text(struct redr_ctx *ctx, const char *text,
-		    int x, int y, int font_size, struct redr_color color);
 void redr_clear_background(struct redr_ctx *ctx, struct redr_color color);
 struct redr_vector2 redr_measure_text(struct redr_ctx *ctx, const char *text,
 				      float font_size, float spacing);
